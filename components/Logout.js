0ccc8bdca0462eb429c5138a48f8f329572325f1
sagarikasonni <102849634+sagarikasonni@users.noreply.github.com>
2024-01-07 14:00:29 +0530

    titi

---
+++ components/Logout.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import firebase from "../src/firebase";
+import firebase from "firebase/app";
+import 'firebase/auth'
+
+const Logout = () => {
+    const handleLogout = async () => {
+        try {
+            await firebase.auth().signOut()
+        }
+        catch (error) {
+            console.error("Error signing out", error.message)
+        }
+    }
+
+    return (
+        <div>
+          <h2>Logout</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+}
+
+export default Logout
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDx1MPgvFGc_DQqKzn_2BU9gaEyd2l-STs",
+  authDomain: "task-manager-f6000.firebaseapp.com",
+  databaseURL: "https://task-manager-f6000-default-rtdb.firebaseio.com",
+  projectId: "task-manager-f6000",
+  storageBucket: "task-manager-f6000.appspot.com",
+  messagingSenderId: "506917379169",
+  appId: "1:506917379169:web:f6ed978dfcf3cd4e475e20",
+  measurementId: "G-7HV2JNZ9CZ",
+};
+
+firebase.initializeApp(firebaseConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
