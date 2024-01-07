9fa5ee3f75b5d74e1374007ab4026deb44b3a724
sagarikasonni <102849634+sagarikasonni@users.noreply.github.com>
2024-01-07 13:42:56 +0530

    woogoo

---
+++ components/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+// import firebase from '../src/firebase/app'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password)
+        }
+        catch (error) {
+            console.error("Error logging in", error.message)
+        }
+    }
+
+    return (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+    );
+}
+
+export default Login
